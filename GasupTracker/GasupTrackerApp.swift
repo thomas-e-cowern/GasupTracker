83c57c390e3f90876fc6337de1dfa09a23eb5d0c
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-06-04 18:21:47 -0400

    add view folder, add home view and history view

---
+++ GasupTracker/GasupTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GasupTrackerApp.swift
+//  GasupTracker
+//
+//  Created by Thomas Cowern on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct GasupTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GasupTracker/Views/HistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryView.swift
+//  GasupTracker
+//
+//  Created by Thomas Cowern on 6/4/24.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text("History View")
+    }
+}
+
+#Preview {
+    HistoryView()
+}
